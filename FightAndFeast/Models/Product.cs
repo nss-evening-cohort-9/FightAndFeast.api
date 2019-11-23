39913ece257fdcd6a6725706965a1c20b9d4a7fd
Lwamtekie <ltekie87@yahoo.com>
2019-11-23 09:53:04 -0600

    fixed the modles for product

--- FightAndFeast/Models/Product.cs
+++ FightAndFeast/Models/Product.cs
@@ -10,7 +10,7 @@ namespace FightAndFeast.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int TypeId { get; set; }
-        public decimal Money { get; set; }
+        public decimal Price { get; set; }
         public string Description { get; set;  }
 
 
