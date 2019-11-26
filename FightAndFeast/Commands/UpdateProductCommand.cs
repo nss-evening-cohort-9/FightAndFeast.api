752f01b519eb75f130abc4f397e4e56838e9ab52
Lwamtekie <ltekie87@yahoo.com>
2019-11-25 20:25:11 -0600

    added delete and update

--- FightAndFeast/Commands/UpdateProductCommand.cs
+++ FightAndFeast/Commands/UpdateProductCommand.cs
@@ -7,5 +7,11 @@ namespace FightAndFeast.Commands
 {
     public class UpdateProductCommand
     {
+              public string Name { get; set; }
+              public int TypeId { get; set; }
+              public decimal Price { get; set; }
+              public string Description { get; set; }
+
     }
 }
+
