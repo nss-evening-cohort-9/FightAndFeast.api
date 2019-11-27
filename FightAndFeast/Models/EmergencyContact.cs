0350bd9bdee24f8bd6b2c53b3db337d3c6d96b5f
Lwamtekie <ltekie87@yahoo.com>
2019-11-26 18:10:47 -0600

    wip

--- FightAndFeast/Models/EmergencyContact.cs
+++ FightAndFeast/Models/EmergencyContact.cs
@@ -7,5 +7,10 @@ namespace FightAndFeast.Models
 {
     public class EmergencyContact
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Relationship { get; set; }
+        public string Phone { get; set; }
     }
 }
