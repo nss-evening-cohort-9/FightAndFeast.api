14079477d37fb4add2444f7bcf090376be4e1fbc
Lwamtekie <ltekie87@yahoo.com>
2019-11-26 18:46:37 -0600

    wip

--- FightAndFeast/Commands/UpdateEmergencyContactCommand.cs
+++ FightAndFeast/Commands/UpdateEmergencyContactCommand.cs
@@ -7,5 +7,11 @@ namespace FightAndFeast.Commands
 {
     public class UpdateEmergencyContactCommand
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Relationship { get; set; }
+        public string Phone { get; set; }
     }
+
 }
+
