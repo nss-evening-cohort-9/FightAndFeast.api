36caf75498493059defe3919e400711b9f7cec2f
Bob <bob.e.baxter@gmail.com>
2019-12-07 16:31:07 -0600

    removed Startup file notes

--- FightAndFeast/Startup.cs
+++ FightAndFeast/Startup.cs
@@ -29,11 +29,6 @@ namespace FightAndFeast
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // to get all settings
-            var authSettings = Configuration.GetSection("AuthenticationSettins");
-            // to get a specific part of settings
-            var connectionString = Configuration.GetValue<string>("ConnectionStrings:FightAndFeast");
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors(options => options.AddPolicy("MyPolicy", builder =>
             {
@@ -41,29 +36,6 @@ namespace FightAndFeast
                        .AllowAnyMethod()
                        .AllowAnyHeader();
             }));
-
-            //    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //        .AddJwtBearer(options =>
-            //        {
-            //            options.IncludeErrorDetails = true;
-            //            options.Authority = authSettings["Authority"];
-            //            options.TokenValidationParameters = new TokenValidationParameters
-            //            {
-            //                ValidateIssuer = true,
-            //                ValidIssuer = authSettings["Issuer"],
-            //                ValidateAudience = true,
-            //                ValidAudience = authSettings["Audience"],
-            //                ValidateLifetime = true
-            //            };
-            //        });
-
-            //    // common services with <servicesToBuild> and (specific instructions)
-            //    services.AddTransient<SqlConnection>(provider => new SqlConnection(connectionString));
-            //        // transient lifecycle, single use, creates a new instance every time the service is requested
-            //    services.AddScoped<ProductRepository>();
-            //        // scoped lifecycle, can be reused safely, uses a single instance and multiple instances will use this single instance
-            //    services.AddSingleton<IConfiguration>(Configuration);
-            //        // singleton lifecycle, only one instance ever (kind of like static)
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
